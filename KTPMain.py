e59418d91a5086c47ad57e189463ec88170743f3
NORTHERN_DEV <97116909+XYZNorthern@users.noreply.github.com>
2023-01-01 13:09:09 +0000

    Update KTPMain.py

--- KTPMain.py
+++ KTPMain.py
@@ -1,25 +1,9 @@
 import hikari
 import crescent
-from songbird import ytdl
-from songbird.hikari import Voicebox
 import flare
 
-@flare.button(label="click", style=hikari.ButtonStyle.SECONDARY)
-async def btntest(ctx: flare.MessageContext):
-
-    await ctx.respond("hi")
-
-bot = crescent.Bot('MTA1ODA2NTIzOTQ0ODQ4NjAxOA.GwGY69.FGbvCSpre61amHjdRD497_M_gWYtmGcOsM-hQY')
+bot = crescent.Bot('YOUR TOKEN')
 flare.install(bot)
-
-@bot.include
-@crescent.command
-async def btntest1(ctx):
-    row = await flare.Row(btntest())
-
-    await ctx.respond("a", component=row)
-
-
 
 bot.plugins.load("extensions.KTPShiftAssignment")
 bot.plugins.load("extensions.KTPMeetings")
