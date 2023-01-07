a54066a23bf4b66f88673f69f732c34cb9f7451f
NORTHERN_DEV <97116909+XYZNorthern@users.noreply.github.com>
2023-01-07 14:46:10 +0000

    Update KTPStrikes.py

--- extensions/KTPStrikes.py
+++ extensions/KTPStrikes.py
@@ -24,7 +24,7 @@ async def add(ctx: crescent.Context, the_member: Atd[hikari.User, "The user who 
     user = ctx.member
     embed = hikari.Embed(title="", description=f":necktie: You have been issued with a __**strike**__ by {ctx.user} for `{reason}`. This strike will expire in **a year's time** in the event that you do not recieve any further infractions. \n \n :helmet_with_cross: If you feel any of these actions are wrong please DM a member of the directorial team or founders.", color="#1a2c23", timestamp=datetime.datetime.now().astimezone())
     await ctx.respond(f"> :white_check_mark: | {the_member} has received a strike.", flags=hikari.MessageFlag.EPHEMERAL)
-    await plugin.app.rest.create_message(channel=1042496469212614686, content=f"> :x: | {ctx.member} has given a strike to {the_member.id} for {reason}")
+    await plugin.app.rest.create_message(channel=CHANNEL_ID, content=f"> :x: | {ctx.member} has given a strike to {the_member.id} for {reason}")
     await the_member.send(embed=embed)
 
 @plugin.include
@@ -41,21 +41,3 @@ async def view(ctx: crescent.Context, user: Atd[hikari.User, "The user who you w
         print(row[0], row[1], row[2])
         embed = hikari.Embed(title=f"{user}'s strikes", description=f"``#{row[0]}`` **STRIKE** | {row[2]}", color="#1a2c23", timestamp=datetime.datetime.now().astimezone())
         await ctx.respond(embed=embed)
-
-@plugin.include
-@crescent.command(name="ktpstrikesremove", description="Remove a strike from a staff member.")
-async def remove(ctx: crescent.Context, user: Atd[hikari.User, "The user who you want to remove the strike from"], case_number: Atd[int, "The case number of the staff member who you are removing the strike."]):
-    con = sqlite3.connect("KTPStrikes.sqlite")
-    cursor = con.cursor()
-
-    user_id = user.id
-    cursor.execute(f"DELETE FROM ktpdatabase WHERE case_number={case_number}")
-    cursor.execute(f"""SELECT case_number FROM ktpdatabase WHERE case_number = ?""", (case_number,))
-    con.commit()
-    remove = cursor.fetchall()
-    for row1 in remove:
-        print(row1[0])
-        embed = hikari.Embed(title="", description=f":necktie: The strike you have been issued with the case number ``#{row1[0]}`` has been removed from your account. \n \n We apologise for the inconvinience.", color="#1a2c23", timestamp=datetime.datetime.now().astimezone())
-        await user.send(embed=embed)
-        await ctx.respond(f":white_check_mark: | {user}'s strike has been removed.", flags=hikari.MessageFlag.EPHEMERAL)
-        await plugin.app.rest.create_message(channel=1042496469212614686, content=f"> :x: | {ctx.member} has removed a strike from {user_id} with case number {row1[0]}") 
